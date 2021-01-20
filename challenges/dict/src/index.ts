68cf8b87616e9387c24df17fed841373f6b9770a
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-01-20 10:26:24 -0800

    chore: added dictionary challenge

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,39 @@
-export type Dict = {};
+export type Dict<T> = {
+	[k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+	dict: Dict<T>, 
+	fn: (arg: T) => S): Dict<S> {
+	const out: Dict<S> = {};
+
+	Object.keys(dict).forEach((dKey, index) => {
+		const item = dict[dKey];
+		if (typeof item !== 'undefined') {
+			out[dKey] = fn(item, index)
+		}
+	});
+	return out;
+}
+
+mapDict({
+	a: 'a',
+	b: 'b'
+}, (str) => ({ val: str }))
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T, R>(
+	dict: Dict<T>,
+	reducer: (acc: R, item: T, idx: string) => R,
+	initialValue: R
+	): R {
+	let currentSum: R = initialValue;
+	Object.keys(dict).forEach(key => {
+		const value = d[key];
+		if (typeof value !== 'undefined') {
+			currentSum = reducer(currentSum, value, key)
+		}
+	})
+	return currentSum;
+}
